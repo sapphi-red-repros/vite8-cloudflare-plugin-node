cc9aaa46d3abda96525f922b5509ab1212441a9f
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-01-16 12:46:27 +0900

    Init

---
+++ worker/index.ts
@@ -0,0 +1,15 @@
+import * as jws from 'jws'
+
+export default {
+  fetch(request) {
+    const url = new URL(request.url)
+
+    if (url.pathname.startsWith('/api/')) {
+      console.log(jws)
+      return Response.json({
+        name: 'Cloudflare',
+      })
+    }
+    return new Response(null, { status: 404 })
+  },
+} satisfies ExportedHandler<Env>
